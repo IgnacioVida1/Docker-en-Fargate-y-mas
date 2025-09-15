a4f60cd532fe21e9eac1969205107220c37ca037
IgnacioVida1 <ignacio.vidal@utec.edu.pe>
2025-09-14 21:36:48 -0500

    Update cdk_stack.py

--- AmazonCDK/cdk_stack.py
+++ AmazonCDK/cdk_stack.py
@@ -6,21 +6,39 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class StudentsApiStack(Stack):
+class ApiStudentsCdkStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        Vpc = ec2.Vpc.from_lookup(self, "MyVPC", vpc_id="vpc-0eb91d56cab4a9114")
+        vpc_ = ec2.Vpc.from_vpc_attributes(
+               self, "MyVPC",
+               vpc_id="vpc-0eb91d56cab4a9114",
+               availability_zones=["us-east-1a", "us-east-1b"],
+               public_subnet_ids=["subnet-0cd04a0321ba04cb9", "subnet-06edefff6ec34b082"])
 
-        cluster = ecs.Cluster(self, "StudentsCluster", vpc=Vpc)
+        cluster = ecs.Cluster(self, "StudentsCluster", vpc=vpc_)
 
         task_definition = ecs.FargateTaskDefinition(
             self, "TaskDef",
             execution_role=iam.Role.from_role_arn(self, "LabRole",
                 "arn:aws:iam::218543369212:role/LabRole",
                 mutable=False),
+            task_role=iam.Role.from_role_arn(self, "LabTaskRole", 
+                "arn:aws:iam::218543369212:role/LabRole", mutable=False),
             cpu=256,
             memory_limit_mib=512,
+        )
+        sg = ec2.SecurityGroup(
+             self, "FargateSG",
+             vpc=vpc_,
+             description="Allow HTTP traffic on port 8000",
+             allow_all_outbound=True,
+        )
+
+        sg.add_ingress_rule(
+             peer=ec2.Peer.any_ipv4(),
+             connection=ec2.Port.tcp(8000),
+             description="Allow public access to port 8000"
         )
 
         task_definition.add_container("StudentsContainer",
@@ -28,12 +46,14 @@ class StudentsApiStack(Stack):
                 "218543369212.dkr.ecr.us-east-1.amazonaws.com/api-students:latest"),
             port_mappings=[ecs.PortMapping(container_port=8000)]
         )
-        Subnet = [ec2.Subnet.from_subnet_id(self, "Subnet1", "subnet-05114a9f87a07611c")]
+        subnet1 = ec2.Subnet.from_subnet_id(self, "Subnet1", "subnet-0cd04a0321ba04cb9")
+        subnet2 = ec2.Subnet.from_subnet_id(self, "Subnet2", "subnet-06edefff6ec34b082")
         ecs.FargateService(
             self, "FargateService",
             cluster=cluster,
             task_definition=task_definition,
             assign_public_ip=True,
             desired_count=1,
-            vpc_subnets=ec2.SubnetSelection(subnets=Subnet)
+            vpc_subnets=ec2.SubnetSelection(subnets=[subnet1, subnet2]),
+            security_groups=[sg]
         )
